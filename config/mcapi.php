1b1b1d85364cd42be99ddc6d3aa6b381013eca6e
Vladimir <dimitrov@vladko.org>
2014-03-05 18:30:30 +0200

    Update mcapi.php
    hide key

--- config/mcapi.php
+++ config/mcapi.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 
 return array(
-	'api_key'	=>	'7d705e17fd1e72fba2da335be8ad5049-us3',
-	'list_id'	=>	'0d2558d669',
+	'api_key'	=>	'',
+	'list_id'	=>	'',
 	);
